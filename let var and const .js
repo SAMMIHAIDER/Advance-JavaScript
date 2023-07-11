97fd2cf7905aee363b10a8ea5916b1c81bd1f2e2
SAMMIHAIDER <samihaider403403@gmail.com>
2023-07-10 22:29:43 -0700

    add new file (let , var, const)

---
+++ let var and const .js
@@ -0,0 +1,41 @@
+// var 
+    //    1> its global scope variable
+    //    2> you can declares a variable name  then  you can declares same  variable name
+    //    3> you can assigned a value 
+
+    // code of (var) variable
+
+    // var variablname = "sam"; // declare a variable and assigned a value
+    // var variablname = 'block' // no any error you can declares same variable name in (var);
+    // variablname='blue';   //  you can assigned a value same variable  name in (var)
+
+
+
+    // let 
+            // 1> it is limited if you make a let variable in any function 
+            //        its work only inside function and not work outside function
+            //  2> you cannot declare a same name variable in(let);
+            //  3> but you can assigned a value
+
+
+    // code of (let) variable
+
+
+        //     let variable = 'sam'; // declare a variable and assigned a value 
+        //     let variable = 'sun';// show error  you can declares same variable name in (let);
+        //     variable='sunny';    // but you can assigned a value in (let) ;  
+
+
+//   const : 
+          //       you can declare only one time variable name and assigned a value only one time
+        //       1> it may be a global scope variable or local scope variable;
+        //       2> you can cannot  declare same variable name ;
+        //       3> and you cannot a assigned a value
+
+// code of (const) variable
+
+//      const san = 'sunny';  // declare a variable and assigned a value 
+//        san = 'sam';          // show error that you cannot assigned a value in (const);
+//        const san = 'Sandy';// show error you can declares same variable name in (const);
+
+               
