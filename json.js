01d9489511aeec1c53d5dfad313449353f9f36fa
SAMMIHAIDER <samihaider403403@gmail.com>
2023-07-13 22:34:04 -0700

    add json file

---
+++ json.js
@@ -0,0 +1,23 @@
+// Json : 
+//         stand for javascript object Notation
+//               1) - json not a language independent
+//               2) - many language are used json formate
+                                         
+//         define Json :-
+//                        is a text formate to store and translating data
+                    
+
+// Syntax of JSON :
+//                  1) - data is in properties and value 
+//                  2) -  data is seperate by commas
+//                  3) -  curly bracket holds Object
+//                  4) - square bracket hold Array
+
+//                  coding :-
+//                               '{
+//                                 "name": "sami",    this code is json form
+//                                 "class": "12th",
+//                                 "roll": "123456",
+//                               }'
+
+                               
